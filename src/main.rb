b0e459557d317c211ae9b699c7b085493f7b1451
Lucas Maximiliano Marino <hello@lucasmarino.me>
2021-04-21 13:31:35 +0200

    feat: implement very basic funcionality

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,16 @@
+secret_number = rand(1..100)
+
 puts "Hello, welcome to Ruby Guessing Game"
 puts "Guess a number between 1 and 100!"
+
 guess = gets.chomp.to_i
+
+if guess <= 1 and guess >= 100
+  puts "Your number is out of range :("
+end
+
+if guess == secret_number
+  puts "Yay, you got it! See you soon!"
+else
+  puts "I'm afraid you did not guess the number. Bye!"
+end
